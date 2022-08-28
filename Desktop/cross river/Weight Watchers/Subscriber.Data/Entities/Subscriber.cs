3c89e8f5aa4994087bf71c31ce985dabc08e2a39
מרים <miriamkaplan123@gmail.com>
2022-08-28 20:01:45 +0300

    addChanges

--- Desktop/cross river/Weight Watchers/Subscriber.Data/Entities/Subscriber.cs
+++ Desktop/cross river/Weight Watchers/Subscriber.Data/Entities/Subscriber.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Subscriber.Data.Entities
 {
+    [Index("Email",IsUnique=true)]
     public class Subscriber
     {
+        [Key]
         public int ID { get; set; }
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+        [Required]
+        [MinLength(8)]  
         public string  Password { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
